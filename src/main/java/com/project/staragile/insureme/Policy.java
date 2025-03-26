4ae4ba4f71ea29e718a5fb6aeed6b435eee79efb
Abdul Faiyaz <abdulfaiyaz.shukor@gmail.com>
2025-03-26 17:19:50 +0530

    fix: stage dependency

--- src/main/java/com/project/staragile/insureme/Policy.java
+++ src/main/java/com/project/staragile/insureme/Policy.java
@@ -1,10 +1,10 @@
 package com.project.staragile.insureme;
 
 import javax.annotation.Generated;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Policy {
